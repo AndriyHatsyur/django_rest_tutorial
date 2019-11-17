f3cf622202a7008ba10f701f4e26c0d6b4dddb3f
Andriy Hatsyur <kanban6@gmail.com>
2019-11-17 22:45:29 +0200

    part 01

--- code/app/urls.py
+++ code/app/urls.py
@@ -1,12 +1,5 @@
-from django.urls import include, path
-from rest_framework import routers
-from app.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('snippets.urls')),
 ]
