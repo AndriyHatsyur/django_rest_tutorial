778306333d48c0089fb9f08000f2e073ac0e9e7e
Andriy Hatsyur <kanban6@gmail.com>
2019-11-24 16:56:39 +0200

    part 05

--- code/snippets/urls.py
+++ code/snippets/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name ='snippets-list' ),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name = 'users-list'),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('snippets/highlight/<int:pk>/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     path('admin/', admin.site.urls),
 ]
 
