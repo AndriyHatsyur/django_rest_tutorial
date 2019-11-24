62c5015fa039af81b7f9b6005bdf9ea2d45d3755
Andriy Hatsyur <kanban6@gmail.com>
2019-11-24 22:23:24 +0200

    part 06

--- code/snippets/urls.py
+++ code/snippets/urls.py
@@ -1,21 +1,17 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
-from django.conf.urls import include
 from django.contrib import admin
 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+
 urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name ='snippets-list' ),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name = 'users-list'),
-    path('user/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('snippets/highlight/<int:pk>/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
 ]
